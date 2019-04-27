648e0c6417e9317819c66dc914e5a34ea4848388
Fabiola Vieyra <fabiolacvieyra@gmail.com>
2019-04-27 20:58:54 +0200

    Adding helmet

--- app/server.js
+++ app/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const helmet = require('helmet');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(helmet());
 app.get('/', function(req, res) {
   res.send('Hello movies! 🎬');
 });
