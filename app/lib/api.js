099c1ce07a26cdfbbc893d8bbe767566d7c255cd
Fabiola Vieyra <fabiolacvieyra@gmail.com>
2019-04-28 20:08:17 +0200

    Normalizing data

--- app/lib/api.js
+++ app/lib/api.js
@@ -1,11 +1,31 @@
 const got = require('got');
 const config = require('../config');
 
+function normalizeData(data) {
+  return new Promise((resolve, reject) => {
+    resolve(
+      Object.keys(data).reduce(function(newData, key) {
+        let newKey = key.toLowerCase();
+        if (data[key] === 'N/A' || (data[key] === 'N/A') === 'false') {
+          data[key] = false;
+        }
+        newData = {
+          ...newData,
+          [newKey]: data[key]
+        };
+        return newData;
+      }, {})
+    ),
+      error => reject(error);
+  });
+}
+
 async function getMovieData(title) {
   const url = `http://www.omdbapi.com/?t=${title}&apikey=${config.omdbKey}`;
   try {
     const response = await got(url);
-    return JSON.parse(response.body);
+    const data = await normalizeData(JSON.parse(response.body));
+    return data;
   } catch (error) {
     console.log(error);
   }
