1b75b1c871658abe6f0bcb6956677a07588538f5
Fabiola Vieyra <fabiolacvieyra@gmail.com>
2019-05-03 22:03:23 +0200

    Adding View engine, views directory and create and update routes

--- app/server.js
+++ app/server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const config = require('./config');
-const app = express();
-const port = config.port;
+const path = require('path');
 const {
   getAllMovies,
   createMovie,
@@ -10,18 +9,25 @@ const {
   updateMovie,
   deleteMovie
 } = require('./lib/controller');
-const db = require('./lib/database');
 const bodyParser = require('body-parser');
 
+const app = express();
+const port = config.port;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(helmet());
 
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
 app.get('/', getAllMovies);
-app.get('/create/:name', createMovie);
 app.get('/movies/:id', getMovieDetail);
-app.get('/movies/:id/update', updateMovie);
 app.get('/movies/:id/delete', deleteMovie);
+app.post('/create', createMovie);
+app.post('/movies/:id/update', updateMovie);
 
 app.listen(port, function() {
-  console.log(`listening from port ${port}`);
+  console.log(`listening on port ${port}`);
 });
